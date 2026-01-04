71e1459730f827e7d1d0082865d09ab6ff36666d
demeteraurion <demeteraurion0@gmail.com>
2026-01-04 08:46:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-garden-app/', // Add this line! It must match your repo name exactly
+  base: '/plant-tracker-app/', // Add this line! It must match your repo name exactly
 })
+
