e9b9427d76641d1c58380ce9412ec2b1dbe28f32
demeteraurion <demeteraurion0@gmail.com>
2026-05-21 19:24:29 -0400

    Fix secret

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDHoxrg07lvHKlPRVVsLyJje385dK-CnW0',
-  authDomain: 'plant-tracker-app-fdf90.firebaseapp.com',
-  projectId: 'plant-tracker-app-fdf90',
-  storageBucket: 'plant-tracker-app-fdf90.firebasestorage.app',
-  messagingSenderId: '996255725521',
-  appId: '1:996255725521:web:f5ba86fded3aa7250fd2f0',
-  measurementId: 'G-P7DHT60FP2',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
